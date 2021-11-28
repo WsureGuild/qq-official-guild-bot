5bf06b73f94e9298431b881a433b65a9d26d221f
Wsure <root@wsure.top>
2021-11-28 14:29:52 +0800

    commit

--- src/main/kotlin/top/wsure/guild/bot/official/dtos/event/guild/member/GuildMemberEvent.kt
+++ src/main/kotlin/top/wsure/guild/bot/official/dtos/event/guild/member/GuildMemberEvent.kt
@@ -1,4 +1,23 @@
 package top.wsure.guild.bot.official.dtos.event.guild.member
+import kotlinx.serialization.Serializable
 
-class GuildMemberAdd {
-}
+import kotlinx.serialization.SerialName
+import top.wsure.guild.bot.official.dtos.User
+
+
+@Serializable
+data class GuildMemberEvent(
+    @SerialName("guild_id")
+    val guildId: String,
+    @SerialName("joined_at")
+    val joinedAt: String,
+    @SerialName("nick")
+    val nick: String,
+    @SerialName("op_user_id")
+    val opUserId: String,
+    @SerialName("roles")
+    val roles: List<String>,
+    @SerialName("user")
+    val user: User
+)
+
