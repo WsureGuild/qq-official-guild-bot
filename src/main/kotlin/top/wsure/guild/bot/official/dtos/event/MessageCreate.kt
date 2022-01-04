a4fed11a0d07498dc06182c17e5d9dc01193b8d3
Wsure <root@wsure.top>
2022-01-04 08:35:20 +0800

    commit

--- src/main/kotlin/top/wsure/guild/bot/official/dtos/event/MessageCreate.kt
+++ src/main/kotlin/top/wsure/guild/bot/official/dtos/event/MessageCreate.kt
@@ -1,4 +1,25 @@
 package top.wsure.guild.bot.official.dtos.event
+import kotlinx.serialization.Serializable
 
-class MessageCreate {
-}
+import kotlinx.serialization.SerialName
+import top.wsure.guild.bot.official.dtos.Author
+import top.wsure.guild.bot.official.dtos.Member
+
+
+@Serializable
+data class MessageCreate(
+    @SerialName("author")
+    val author: Author,
+    @SerialName("channel_id")
+    val channelId: String,
+    @SerialName("content")
+    val content: String,
+    @SerialName("guild_id")
+    val guildId: String,
+    @SerialName("id")
+    val id: String,
+    @SerialName("member")
+    val member: Member,
+    @SerialName("timestamp")
+    val timestamp: String
+)
