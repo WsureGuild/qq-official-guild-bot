c9960f172ffb699f00f19829f284db3c7dd4b954
swang23 <swang23@stubhubcorp.com>
2022-01-06 17:31:50 +0800

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.0"
     application
-//    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.0"
     kotlin("plugin.serialization") version "1.6.0"
 }
 
@@ -15,13 +14,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.2-native-mt")
-    implementation("top.wsure.guild:wsure-guild-common:1.0-SNAPSHOT")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
-    implementation("com.squareup.okhttp3:okhttp:4.9.2")
-    implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("ch.qos.logback:logback-core:1.2.6")
-    implementation("ch.qos.logback:logback-classic:1.2.6")
+    api("top.wsure.guild:wsure-guild-common:1.0-SNAPSHOT")
+    implementation("ch.qos.logback:logback-core:1.2.10")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
 
     testImplementation(kotlin("test"))
 }
